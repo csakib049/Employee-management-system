55d6b46caf7daa0aebccd5e533c1ae23f4df944f
Md.Sakib Chowdhury <csakib049@gmail.com>
2025-11-20 13:12:15 +0600

    updating

--- ems/src/App.jsx
+++ ems/src/App.jsx
@@ -7,29 +7,45 @@ import { AuthContext } from './context/AuthProvider'
 
 const App = () => {
 
-  const [user,setUser]=useState(null)
+  const [user, setUser] = useState(null)
+  const authData = useContext(AuthContext);
 
 
-  const handleLogin = (email,password)=>{
-    if(email== 'admin@me.com' && password=='123'){
-      setUser('admin');
-    }else if(email== 'user@me.com' && password=='123'){
-      setUser('employee');
+  useEffect(() => {
+
+    if(authData){
+      const loggedInUser = localStorage.getItem("loggedInUser")
+      if(loggedInUser){
+        setUser(loggedInUser.role)
+      }
+
     }
-    else{
+
+  }, [authData]);
+
+
+  const handleLogin = (email, password) => {
+    if (email == 'admin@me.com' && password == '123') {
+      setUser('admin');
+      localStorage.setItem('loggedInUser',JSON.stringify({role:'admin'}))
+    } else if (authData && authData.employees.find((e) => email == email && e.password == password)) {
+      setUser('employee');
+      localStorage.setItem('loggedInUser',JSON.stringify({role:'employee'}))
+    }
+    else {
       alert("invalid credentials")
     }
   }
 
 
-  const data =useContext(AuthContext);
+
 
 
   return (
     <div className='bg-stone-600 '>
-    
-      {!user? <Login handleLogin={handleLogin} /> : ''}
-      {user =='admin' ? <AdminDashboard/> : <EmployeeDashboard/>}
+
+      {!user ? <Login handleLogin={handleLogin} /> : ''}
+      {user == 'admin' ? <AdminDashboard /> : <EmployeeDashboard />}
       {/* <EmployeeDashboard/> */}
       {/* <AdminDashboard/> */}
 
